90149d70e2b229f86f3ee54cdcdbf3761d036329
ramenjuniti <ramenjuniti@gmail.com>
2021-05-17 22:19:38 +0900

    コンストラクタ使う

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -3,9 +3,9 @@ package money;
 public class Dollar {
     int amount;
     Dollar(int amount) {
-
+        this.amount = amount;
     }
     void times(int multiplier) {
-        amount = 5 * 2;
+        amount *= multiplier;
     }
 }
