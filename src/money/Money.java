60ad4ce9b40deac71b76e63c6acecc1877ebe980
ramenjuniti <ramenjuniti@gmail.com>
2021-05-23 18:47:21 +0900

    Moneyのコンストラクタ作成

--- src/money/Money.java
+++ src/money/Money.java
@@ -3,6 +3,10 @@ package money;
 abstract class Money {
     protected int amount;
     protected String currency;
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     abstract Money times(int multiplier);
     String currency() {
         return currency;
@@ -12,9 +16,9 @@ abstract class Money {
         return amount == money.amount && getClass().equals(money.getClass());
     }
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 }
