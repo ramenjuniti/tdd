ff5dcce4f2517a96cf2c943cd38a00a489b4b110
ramenjuniti <ramenjuniti@gmail.com>
2021-05-29 20:04:18 +0900

    Add setUp

--- 2/xunit.py
+++ 2/xunit.py
@@ -1,7 +1,10 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method() 
 
@@ -9,10 +12,21 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+    def setUp(self):
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
